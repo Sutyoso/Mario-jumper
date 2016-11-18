10dc1eb0d8336cee48654aa796f5cee06d89640c
adrianstefanust <astefanus44@gmail.com>
2016-11-18 13:33:23 +0700

    uhh

--- Assets/Script/CharacterScript.cs
+++ Assets/Script/CharacterScript.cs
@@ -1,113 +1,151 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class CharacterScript : MonoBehaviour {
+/// <summary>
+/// kelas ini berguna untuk mengatur semua pergerakan dari character
+/// </summary>
+public class CharacterScript : MonoBehaviour
+{
 
     [SerializeField]
     private float jumpForce = 300f;
-        
+
     private bool isGrounded;
     private bool jump;
     private bool slide;
     private Animator myAnimator;
     private Rigidbody2D myRigidbody;
 
-	[SerializeField]
-	private Transform[] groundPoints;
+    [SerializeField]
+    private Transform[] groundPoints;
 
-	[SerializeField]
-	private float groundRadius;
+    [SerializeField]
+    private float groundRadius;
 
-	[SerializeField]
-	private LayerMask whatIsGround;
+    [SerializeField]
+    private LayerMask whatIsGround;
 
-	// Use this for initialization
-	/*
-	 * pada method start ini terdapat constructor yang berguna untuk menginisialisasi animator dan rigidbody2d
-	*/
-	void Start () {
-		myAnimator = GetComponent<Animator> ();
-		this.myRigidbody = GetComponent<Rigidbody2D> ();
-        
+    /// <summary>
+    /// pada method start ini terdapat constructor yang berguna untuk menginisialisasi animator dan rigidbody2d
+    /// </summary>
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        this.myRigidbody = GetComponent<Rigidbody2D>();
+
     }
 
-	// Update is called once per frame
-	void Update () {
-		handleInput ();
-	}
+    /// <summary>
+    ///    Update is called once per frame
+    /// </summary>
 
-	void FixedUpdate(){
-		isGrounded = groundedChecker ();
-  
-		handleMovement ();
+    void Update()
+    {
+        handleInput();
+    }
 
-		resetValue ();
-	}
+    void FixedUpdate()
+    {
+        isGrounded = groundedChecker();
 
-	void handleInput(){
-		if (Input.GetKeyDown(KeyCode.S)) {
-			slide = true;
-		} else if (Input.GetKey(KeyCode.W)) {
-			jump = true;
-		}
-	}
-	/*
-	 * dalam method handleMovement berisi command command untuk lompat dan slide
-	 * dalam method ini juga berisi command command untuk mengganti animasi
-	*/
+        handleMovement();
 
-	void handleMovement(){
-		if (jump && isGrounded) {
+        resetValue();
+    }
+    /// <summary>
+    /// mengatur input
+    /// </summary>
+    void handleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            slide = true;
+
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+
+            jump = true;
+        }
+    }
+    /// <summary>
+    /// dalam method handleMovement berisi command command untuk lompat dan slide
+    /// dalam method ini juga berisi command command untuk mengganti animasi
+    /// </summary>
+
+    void handleMovement()
+    {
+        if (jump && isGrounded)
+        {
             GetComponent<AudioSource>().Play();
             isGrounded = false;
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(0,jumpForce));
-			this.myAnimator.SetBool ("isJumping", true);
-		} else if (jump && !isGrounded) {
-			isGrounded = true;
-			jump = false;
-			this.myAnimator.SetBool ("isJumping", false);
-		}
-		if (slide && !this.myAnimator.GetCurrentAnimatorStateInfo (0).IsName ("Slide")) {
-			this.myAnimator.SetBool ("isSlide", true);
-			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, -jumpForce));
-		} else if (!this.myAnimator.GetNextAnimatorStateInfo (0).IsName ("Slide")) {
-			this.myAnimator.SetBool ("isSlide", false);
-		}
-	}
-	/*
-	 * method ini berisi command command untuk mengecek apakah character tersebut menginjak tanah atau tidak 
-	*/
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+            this.myAnimator.SetBool("isJumping", true);
+        }
+        else if (jump && !isGrounded)
+        {
+            isGrounded = true;
+            jump = false;
+            this.myAnimator.SetBool("isJumping", false);
+        }
+        if (slide && !this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Slide"))
+        {
+            this.myAnimator.SetBool("isSlide", true);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -jumpForce));
+        }
+        else if (!this.myAnimator.GetNextAnimatorStateInfo(0).IsName("Slide"))
+        {
+            this.myAnimator.SetBool("isSlide", false);
+        }
+    }
+    /// <summary>
+    ///  method ini berisi command command untuk mengecek apakah character tersebut menginjak tanah atau tidak 
+    /// </summary>
+    /// <returns>
+    /// mengembalikan true jika collider menyentuh tanah dan false jika tidak menyentuh tanah
+    /// </returns>
 
-	private bool groundedChecker(){
-		if (myRigidbody.velocity.y <= 0) {
-			foreach (Transform point in groundPoints) {
-				Collider2D[] coll = Physics2D.OverlapCircleAll (point.position, groundRadius,whatIsGround);
+    private bool groundedChecker()
+    {
+        if (myRigidbody.velocity.y <= 0)
+        {
+            foreach (Transform point in groundPoints)
+            {
+                Collider2D[] coll = Physics2D.OverlapCircleAll(point.position, groundRadius, whatIsGround);
 
-				for (int i = 0; i < coll.Length; i++) {
-					if (coll [i].gameObject != gameObject) {
-						return true;
-					}
-				}
-			}
-		}
-		return false;
-	}
+                for (int i = 0; i < coll.Length; i++)
+                {
+                    if (coll[i].gameObject != gameObject)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    /// <summary>
+    /// 
+    /// mereset value dari jump dan slide
+    /// </summary>
+    void resetValue()
+    {
+        this.jump = false;
+        this.slide = false;
 
-	void resetValue(){
-		this.jump = false;
-		this.slide = false;
+    }
 
-	}
+    /// <summary>
+    /// method ini berisi instruksi jika character tabrakan dengan obstacle maka obstacle dan character akan difreeze
+    /// </summary>
+    /// <param name="collider">
+    /// merupakan collider dari obstacle
+    /// </param>
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.name == "obs")
+        {
 
-	/*
-	 * method ini berisi instruksi jika character tabrakan dengan obstacle maka obstacle dan character akan difreeze
-	*/
-	void OnCollisionEnter2D(Collision2D collider)
-	{
-		if (collider.gameObject.name == "obs")
-		{
-		
-			collider.gameObject.GetComponent<ObstacleMovement> ().isRun = false;
+            collider.gameObject.GetComponent<ObstacleMovement>().isRun = false;
             Application.LoadLevel("Game Over");
             collider.gameObject.GetComponent<ObstacleMovement>().isRun = true;
             //collider.gameObject.GetComponent<char().isRun = false;
--- Assets/Script/ScoreManager.cs
+++ Assets/Script/ScoreManager.cs
@@ -2,9 +2,10 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class ScoreManager : MonoBehaviour {
-    
-	public Text scoreText;
+public class ScoreManager : MonoBehaviour
+{
+
+    public Text scoreText;
     public Text hiScoreText;
     [SerializeField]
     public float scoreCount;
@@ -16,18 +17,20 @@ public class ScoreManager : MonoBehaviour {
     public bool scoreIncreasing;
 
 
-    void Start() {
-        if (PlayerPrefs.GetFloat("HighScore") != null) {
+    void Start()
+    {
+        if (PlayerPrefs.GetFloat("HighScore") != null)
+        {
             hiScoreCount = PlayerPrefs.GetFloat("HighScore");
         }
 
     }
-	/*
+    /*
 	 * method ini berisi command untuk menambahkan score 
 	*/
     void Update()
     {
-       
+
         if (scoreIncreasing)
         {
             scoreCount += pointsPerSecond * Time.deltaTime;
@@ -37,10 +40,10 @@ public class ScoreManager : MonoBehaviour {
             hiScoreCount = scoreCount;
             PlayerPrefs.SetFloat("HighScore", hiScoreCount);
         }
-       
+
         scoreText.text = "Score: " + Mathf.Round(scoreCount);
         hiScoreText.text = "High Score: " + Mathf.Round(hiScoreCount);
-        if (Mathf.Round(scoreCount) % 100 == 0&& Mathf.Round(scoreCount)!=0)
+        if (Mathf.Round(scoreCount) % 100 == 0 && Mathf.Round(scoreCount) != 0)
         {
             GetComponent<AudioSource>().Play();
         }
